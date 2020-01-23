974182ce3ae75b6cbd0edd6c4c66ebfff94c31d1
mhutch2b87 <mhutch2b87@gmail.com>
2020-01-23 12:16:05 -0600

    updated to class work-functioning correctly

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,29 @@
+function f1Data(){
+    let Year = $( "#submitForm input[name=Year]" ).val()
+    let Lap = $("#submitForm input[name=Lap]" ).val()
+    console.log(Year, Lap)
 
-$("#submitButton").click(function (){
-    console.log("hello")
-})
+    $.get('https://ergast.com/api/f1/' + Year + '/' + Lap + '/' + 'driverStandings.json', function(data){
+        let common = data.MRData.StandingsTable.StandingsLists[0]
 
-$("#submitButton").click(function (){
-    console.log("hello")
-    $.get('https://ergast.com/api/f1/2019/1/driverStandings.json', function (data){
-         console.log(data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].position)
-         console.log(data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver.nationality)
+        //For Loop For the Table
+        for(let i=0; i < 8; i++){
+            let position = common.DriverStandings[i].position
+            let name = common.DriverStandings[i].Driver.givenName
+            let nationality = common.DriverStandings[i].Driver.nationality
+            let sponser = common.DriverStandings[i].Constructors[0].name
+            let points = common.DriverStandings[i].points
+
+            //input data into table 
+
+            $("#position" + i.toString()).text(position)
+            $("#driverName" + i.toString()).text(name)
+            $("#nationality" + i.toString()).text(nationality)
+            $("#sponser" + i.toString()).text(sponser)
+            $("#points" + i.toString()).text(points)
+
+        }
     })
-})
+
+}
+    
